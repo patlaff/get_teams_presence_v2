18f9052ebfa1a7b14a9eaec10b3edb4917011ff6
Patrick Lafferty <patlaff728@gmail.com>
2024-01-05 15:31:00 -0500

    init commit before forking

--- app.py
+++ app.py
@@ -2,15 +2,44 @@ import identity.web
 import requests
 from flask import Flask, redirect, render_template, request, session, url_for
 from flask_session import Session
+from celery import Celery
+import time
+#from sense_hat import SenseHat
 
 import app_config
 
 __version__ = "0.7.0"  # The version of this sample, for troubleshooting purpose
 
+# Sense Hat stuff:
+# sense = SenseHat()
+# sense.low_light = True
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
 app = Flask(__name__)
 app.config.from_object(app_config)
 assert app.config["REDIRECT_PATH"] != "/", "REDIRECT_PATH must not be /"
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
 Session(app)
+
+celery = make_celery(app)
 
 # This section is needed for url_for("foo", _external=True) to automatically
 # generate http scheme when this sample is running on localhost,
@@ -34,7 +63,7 @@ def login():
         scopes=app_config.SCOPE, # Have user consent to scopes during log-in
         redirect_uri=url_for("auth_response", _external=True), # Optional. If present, this absolute URL must match your app's redirect_uri registered in Azure Portal
         prompt="select_account",  # Optional. More values defined in  https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
-        ))
+    ))
 
 
 @app.route(app_config.REDIRECT_PATH)
@@ -61,18 +90,38 @@ def index():
     return render_template('index.html', user=auth.get_user(), version=__version__)
 
 
-@app.route("/call_downstream_api")
-def call_downstream_api():
-    token = auth.get_token_for_user(app_config.SCOPE)
-    if "error" in token:
-        return redirect(url_for("login"))
-    # Use access token to call downstream api
+@celery.task()
+def getPresence(token):
     api_result = requests.get(
         app_config.ENDPOINT,
         headers={'Authorization': 'Bearer ' + token['access_token']},
         timeout=30,
     ).json()
-    return render_template('display.html', result=api_result)
+    # Parse Graph Response to get current User Activity
+    activity = api_result['activity']
+    # Present message if user is busy, otherwise sleep for 60 seconds
+    if activity in ['InACall', 'InAConferenceCall', 'Presenting']:
+        print("ON AIR")
+        # sense.show_message(
+        #     "On Air", 
+        #     text_colour=(0,0,255), 
+        #     scroll_speed=0.2
+        # )
+        # sense.clear()
+    else:
+        print("Not Busy")
+        time.sleep(60)
+    return activity
+
+        
+@app.route("/get_presence")
+def get_presence():
+    token = auth.get_token_for_user(app_config.SCOPE)
+    if "error" in token:
+        return redirect(url_for("login"))
+    # Use access token to call downstream api
+    presence = getPresence(token)
+    return render_template('display.html', result=presence)
 
 
 if __name__ == "__main__":
